f01a991e1d9f05f21bd06f77bd04f54a3a91d3d9
Louis5071 <ramspottlouis@googlemail.com>
2025-03-19 20:15:53 -0700

    r

---
+++ src/Lab3-A.cpp
@@ -0,0 +1,157 @@
+// Link to the video of the project:
+// 
+
+#include <Arduino.h>
+//#include <Wire.h> // I2C library
+//#include <SPI.h>
+//#include <HttpClient.h>
+//#include <Ethernet.h>
+//#include <EthernetClient.h>
+#include <WiFi.h>
+#include <HttpClient.h>
+
+
+//constants for the pins where sensors are plugged into.
+const int sensorPin = 2;
+
+// variables
+unsigned long startTime;
+
+
+// This example downloads the URL "http://arduino.cc/"
+
+char ssid[] = "[YOUR_SSID]";    // your network SSID (name) 
+char pass[] = "[YOUR_PASSWORD]"; // your network password (use for WPA, or use as key for WEP)
+
+// Name of the server we want to connect to
+const char kHostname[] = "worldtimeapi.org";
+// Path to download (this is the bit after the hostname in the URL
+// that you want to download
+const char kPath[] = "/api/timezone/Europe/London.txt";
+
+// Number of milliseconds to wait without receiving any data before we give up
+const int kNetworkTimeout = 30*1000;
+// Number of milliseconds to wait if no data is available before trying again
+const int kNetworkDelay = 1000;
+
+
+
+
+void setup() {
+  delay(3000);
+  
+  // Test Serial Monitor
+  Serial.begin(115200);
+  Serial.println("Programm Start");
+
+  startTime = millis();
+
+  pinMode(sensorPin, INPUT);
+
+
+  // We start by connecting to a WiFi network
+  delay(1000);
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println("MAC address: ");
+  Serial.println(WiFi.macAddress());
+
+}
+
+void loop() {
+  int err =0;
+  
+  WiFiClient c;
+  HttpClient http(c);
+  
+  err = http.get(kHostname, kPath);
+  if (err == 0)
+  {
+    Serial.println("startedRequest ok");
+
+    err = http.responseStatusCode();
+    if (err >= 0)
+    {
+      Serial.print("Got status code: ");
+      Serial.println(err);
+
+      // Usually you'd check that the response code is 200 or a
+      // similar "success" code (200-299) before carrying on,
+      // but we'll print out whatever response we get
+
+      err = http.skipResponseHeaders();
+      if (err >= 0)
+      {
+        int bodyLen = http.contentLength();
+        Serial.print("Content length is: ");
+        Serial.println(bodyLen);
+        Serial.println();
+        Serial.println("Body returned follows:");
+      
+        // Now we've got to the body, so we can print it out
+        unsigned long timeoutStart = millis();
+        char c;
+        // Whilst we haven't timed out & haven't reached the end of the body
+        while ( (http.connected() || http.available()) &&
+               ((millis() - timeoutStart) < kNetworkTimeout) )
+        {
+            if (http.available())
+            {
+                c = http.read();
+                // Print out this character
+                Serial.print(c);
+               
+                bodyLen--;
+                // We read something, reset the timeout counter
+                timeoutStart = millis();
+            }
+            else
+            {
+                // We haven't got any data, so let's pause to allow some to
+                // arrive
+                delay(kNetworkDelay);
+            }
+        }
+      }
+      else
+      {
+        Serial.print("Failed to skip response headers: ");
+        Serial.println(err);
+      }
+    }
+    else
+    {    
+      Serial.print("Getting response failed: ");
+      Serial.println(err);
+    }
+  }
+  else
+  {
+    Serial.print("Connect failed: ");
+    Serial.println(err);
+  }
+  http.stop();
+
+  // And just stop, now that we've tried a download
+  while(1);
+}
+  
+
+
+
+
+
+  
